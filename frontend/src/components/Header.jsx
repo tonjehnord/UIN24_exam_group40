5e35fe14d16c7afaa99a09bfa6283e567cce69e9
tonjehnord <tonjnord@gmail.com>
2024-05-19 18:02:44 +0200

    oppdatert header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,15 @@
+import { PiUserCircle } from "react-icons/pi";
+import { BiCameraMovie } from "react-icons/bi";
+
 export default function Header() {
     return (
         <header>
             <nav>
                 <ul>
-                    <li>HEI</li>
+                    <li className="logo">What To See?</li>
+                    <li><BiCameraMovie /> Hva skal jeg se?</li>
+                    <li>Bla gjennom sjangere</li>
+                    <li><PiUserCircle /> Bruker</li>
                 </ul>
             </nav>
         </header>
